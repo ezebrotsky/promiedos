4b16b68c824d01601701937e4385bc358aaba033
Eze Brotsky <ezequielbrotsky@gmail.com>
2024-11-14 20:19:18 +0200

    Promiedos main page

---
+++ main.py
@@ -0,0 +1,92 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+def promiedos() -> dict:
+    """
+        Scrapes a website and extracts various elements.
+        
+        Parameters:
+        url (str): The URL of the website to scrape.
+        
+        Returns:
+        dict: A dictionary containing the extracted elements.
+    """
+    # Send a GET request to the URL
+    response = requests.get('https://www.promiedos.com.ar')
+
+    # Parse the HTML content using Beautiful Soup
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # get leagues
+    leagues = soup.find_all(id="fixturein")
+
+    games_results = {}
+    
+    for league in leagues:
+        league_results = []
+
+        # title
+        league_title_html = league.find('tr', {"class": 'tituloin'})
+        league_title = league_title_html.find('a').text.strip()
+        print(league_title)
+
+        # get games per league
+        games = league.find_all('tr', {"name":["nvp", "vp"]})
+        
+        for game in games:
+            # print(game)
+
+            ## times
+            time = ''
+            # if not started
+            game_time = game.find('td', {"class": 'game-time'})
+            if game_time:
+                time = game_time.text.strip()
+            
+            # if playing
+            game_play = game.find('td', {"class": 'game-play'})
+            if game_play:
+                time = game_play.text.strip()
+            
+            # if ended
+            game_fin = game.find('td', {"class": 'game-fin'})
+            if game_fin:
+                time = game_fin.text.strip()
+
+            ## teams
+            # results
+            result_local = game.find('td', {"class": 'game-r1'})
+            if result_local:
+                result_local = result_local.text.strip()
+            
+            result_visitor = game.find('td', {"class": 'game-r2'})
+            if result_visitor:
+                result_visitor = result_visitor.text.strip()
+
+            # teams
+            teams = game.find_all('td', {"class": 'game-t1'})
+            if len(teams) > 0:
+                team_local = teams[0].text.strip()
+                team_visitor = teams[1].text.strip()
+
+                if ':' in time:
+                    formatted_time = datetime.combine(datetime.today(), datetime.strptime(time, '%H:%M').time(), ZoneInfo('America/Argentina/Buenos_Aires')).isoformat()
+                else:
+                    formatted_time = time
+
+                league_results.append({
+                    'local': { 'team': team_local, 'result': result_local },
+                    'visitor': { 'team': team_visitor, 'result': result_visitor },
+                    'time': formatted_time
+                })
+                
+                print(f"- Time: {time} [Argentina] | {team_local} ({result_local}) Vs. {team_visitor} ({result_visitor})")
+            
+        games_results[league_title] = league_results
+
+    return games_results
+
+def lambda_handler(event, context):
+    return promiedos()
---
+++ main.tf
@@ -0,0 +1,226 @@
+### Main
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+### ECR
+resource "aws_ecr_repository" "promiedos" {
+  name                 = "promiedos"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+### Lambda
+resource "aws_s3_bucket" "lambda" {
+  bucket = "promiedos-lambda"
+}
+
+data "aws_iam_policy_document" "assume_lambda_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_lambda_role.json
+}
+
+resource "aws_lambda_function" "promiedos_lambda" {
+  function_name = "promiedos"
+  role          = aws_iam_role.iam_for_lambda.arn
+  package_type  = "Image" 
+  image_uri     = "${aws_ecr_repository.promiedos.repository_url}:latest"
+}
+
+### Pipeline
+resource "aws_codepipeline" "codepipeline" {
+  name     = "promiedos"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        ConnectionArn    = aws_codestarconnections_connection.personal.arn
+        FullRepositoryId = "ezebrotsky/promiedos"
+        BranchName       = "main"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.build_promiedos_lambda.name
+      }
+    }
+  }
+}
+
+resource "aws_codestarconnections_connection" "personal" {
+  name          = "personal"
+  provider_type = "GitHub"
+}
+
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket = "promiedos-codepipeline"
+}
+
+resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_pab" {
+  bucket = aws_s3_bucket.codepipeline_bucket.id
+}
+
+data "aws_iam_policy_document" "assume_pipeline_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name               = "codepipeline-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_pipeline_role.json
+}
+
+data "aws_iam_policy_document" "codepipeline_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObjectAcl",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.codepipeline_bucket.arn,
+      "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+    ]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["codestar-connections:UseConnection"]
+    resources = [aws_codestarconnections_connection.personal.arn]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name   = "codepipeline_policy"
+  role   = aws_iam_role.codepipeline_role.id
+  policy = data.aws_iam_policy_document.codepipeline_policy.json
+}
+
+
+
+data "aws_iam_policy_document" "assume_codebuild_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "codebuild_promiedos_role" {
+  name               = "codebuild_promiedos_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_codebuild_role.json
+}
+
+resource "aws_codebuild_project" "build_promiedos_lambda" {
+  name          = "codebuild_promiedos_lambda"
+  build_timeout = "15"
+  service_role  = "${aws_iam_role.codebuild_promiedos_role.arn}"
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    // https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html
+    image           = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = templatefile(
+      "./buildspecs/promiedos_lambda.yml",
+      {
+        account = var.aws_account_id
+      }
+    )
+  }
+}
